27684548d288d61c44df485b6e9c693ee7f5c907
Juned <hussainjuned99@gmail.com>
2019-06-16 14:28:00 +0600

    search page: showing colour bg instead of hex code.

--- storage/framework/views/259bc8692f6d173e53a271ea05a88ec1ed1cc8cc.php
+++
@@ -1,5 +0,0 @@
-<footer style="background: #3e3e3e; padding-top: 30px; padding-bottom: 30px;"><div class="container" style="display: flex; justify-content: space-between;">
-        <p style="color: #fefefe; font-size: 12px; margin-bottom: 0;">Developed By: <span><a href="https://hussain-juned.com">Hussain Juned</a></span></p>
-        <p style="color: #fefefe; font-size: 12px; margin-bottom: 0;">Server Time:  <span><?php echo e(Carbon\Carbon::now()); ?></span></p>
-    </div>
-</footer><?php /**PATH C:\Users\Juned\PhpstormProjects\flowerapp\resources\views/partials/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/2e40b957985c4ad4f9f4e5738323e6b979c6c835.php
+++
@@ -1,8 +0,0 @@
-<?php if(session()->has('message')): ?>
-    <div class="container my-5">
-        <div class="alert alert-success">
-            <?php echo e(session()->get('message')); ?>
-
-        </div>
-    </div>
-<?php endif; ?><?php /**PATH C:\Users\Juned\PhpstormProjects\flowerapp\resources\views/partials/success_message.blade.php ENDPATH**/ ?>
