a6adeb6bc676031782d474925fdc82a554e534f3
Juned <hussainjuned99@gmail.com>
2019-06-21 15:50:08 +0600

    top navbar search: loading animation and no result found text added.

--- resources/js/components/MainNavSearch.vue
+++ resources/js/components/MainNavSearch.vue
@@ -4,7 +4,14 @@
         <input type="text" class="form-control" id="main_search_inp" v-model="keyword">
         <span id="main_search_x" v-on:click="main_search_x"><i class="fas fa-times"></i></span>
 
-        <ul id="main_s_result" v-if="products">
+        <ul id="main_s_result">
+            <li v-if="loading" style="font-size: 14px; text-transform: capitalize">
+                <img src="images/icons/loader.gif" alt="loading..." />
+                Loading.....
+            </li>
+
+            <li v-if="no_result" class="no_result_found"><a href="#">No result found</a></li>
+
             <li v-for="product in products">
                 <a v-bind:href="'products/' + product.id">
                     <div class="row">
@@ -33,32 +40,44 @@
         data() {
             return {
                 keyword: '',
-                products: []
+                products: [],
+                loading: false,
+                no_result: false,
             }
         },
         watch: {
             keyword(after, before) {
                 this.fetch();
-                console.log(this.keyword);
+                // console.log(this.keyword);
             },
         },
         methods: {
             fetch() {
                 if (this.keyword) {
+                    this.loading = true;
                     axios.get('/api/flower', {
                         params: {
                             keywords: this.keyword, delivery_date: this.delivery_date,
                         }
                     }).then(response => {
+                        this.loading = false;
                         this.products = response.data.data;
+
+                        if (!$.trim(this.products)) {
+                            // $('#main_s_result').html('<li> No result found</li>');
+                            // console.log('no result');
+                            this.no_result = true;
+                        }
 
                         // this.page_data = response.data;
                         // console.log('np url: ' + this.page_data.next_page_url);
                         // $('.result_count').text(response.data.total);
                     }).catch(error => {
+                        this.loading = false;
                     });
                 } else {
                     this.products = [];
+                    this.no_result = false;
                 }
 
 
@@ -69,6 +88,7 @@
                 $('#fixed_nav').removeClass('show_search');
                 this.keyword = '';
                 this.products = [];
+                this.no_result = false;
             }
         },
         props: ['delivery_date']
@@ -76,5 +96,7 @@
 </script>
 
 <style scoped>
-
+    .no_result_found {
+        text-transform: initial;
+    }
 </style>
